ddc4667bd242d7a7fd60e2e4393c7fe6ec33cb7a
“anurag03-10” <1928216@kiit.ac.in>
2026-04-26 01:23:20 +0530

    chore: setup project structure and ignore build/env files

---
+++ servers/terminal_server/main.py
@@ -0,0 +1,29 @@
+import os
+import subprocess
+from mcp.server.fastmcp import FastMCP
+
+
+mcp = FastMCP("terminal")
+DEFAULT_WORKSPACE = os.path.expanduser("/Users/anuragsingh/Krish Naik GenAI/mcp_bt/workspace")
+
+
+
+@mcp.tool()
+async def run_command(command: str) -> str:
+    """Run a terminal command inside the workspace directory.
+    If a terminal command is executed, the output will be returned as a string. If an error occurs, the error message will be returned instead.
+    
+    Args:
+    the terminal command to be executed as a string.
+    Returns:
+        The command output or error message as a string.
+    """
+    try:
+        result = subprocess.run(command, shell = True, cwd = DEFAULT_WORKSPACE, capture_output=True, text=True)
+        return result.stdout or result.stderr
+    except Exception as e:
+        return str(e)
+    
+
+if __name__ == "__main__":
+    mcp.run(transport='stdio')
